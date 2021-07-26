15460eb9f98fa92d01047e290d9750c2e57c0594
german1190 <adalg.design@gmail.com>
2021-07-26 02:00:18 -0400

    back button oculto

--- alert.js
+++ alert.js
@@ -1,4 +1,4 @@
   function miFunc() {
-    alert('Has llamado a un camarero en unos minutos te atendera, gracias');
+    alert('Has llamado a un camarero en unos minutos te atendera, gracias.');
   }
  
