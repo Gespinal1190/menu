a69af19beb646749d12431e8ba8a320077aeaa8e
german1190 <adalg.design@gmail.com>
2021-07-26 02:37:05 -0400

    cambios

--- alert.js
+++ alert.js
@@ -1,4 +1,10 @@
-  function miFunc() {
+
+
+function miFunc() {
     alert('Has llamado a un camarero en unos minutos te atendera, gracias.');
-  }
+};
+ 
+function miFunc2() {
+    alert('Es solo una prueba');
+};
  
